1853ef82bd84ce278aba8eae5a1e153787a13ced
fine <otxw0125@gmail.com>
2025-05-05 18:32:09 +0900

    auth 수정중 2

--- backend/dist/users/users.module.js
+++ backend/dist/users/users.module.js
@@ -15,8 +15,10 @@ let UsersModule = class UsersModule {
 exports.UsersModule = UsersModule;
 exports.UsersModule = UsersModule = __decorate([
     (0, common_1.Module)({
+        imports: [],
+        controllers: [users_controller_1.UsersController],
         providers: [users_service_1.UsersService],
-        controllers: [users_controller_1.UsersController]
+        exports: [users_service_1.UsersService]
     })
 ], UsersModule);
 //# sourceMappingURL=users.module.js.map
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,9 +1,17 @@
+// src/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { UsersController } from './users.controller'; // UsersController가 있다면 import
+// DatabaseModule이나 MongooseModule.forFeature 등 필요한 import 추가
 
 @Module({
-  providers: [UsersService],
-  controllers: [UsersController]
+  imports: [
+    // 여기에 MongooseModule.forFeature(...) 또는 다른 필요한 모듈 import
+  ],
+  controllers: [UsersController], // UsersController가 있다면 포함
+  providers: [UsersService],       // UsersService를 이 모듈의 provider로 등록
+  exports: [UsersService]         // <--- 이 줄을 추가하세요!
+                                  // UsersService를 다른 모듈(예: AuthModule)에서
+                                  // 주입받아 사용할 수 있도록 내보냅니다.
 })
 export class UsersModule {}
